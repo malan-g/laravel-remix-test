3d8a4e1fc1cdf4de129ccb838a0f4760dd8ff1b6
Malan Gunaratne <malan-g@outlook.com>
2024-04-25 14:00:44 +1200

    load tasks via. fetch

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,6 +9,25 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async () => {
+  // assuming the backend is running @ localhost:8000 & allows Access from this origin
+  const res = await fetch("http://127.0.0.1:8000/api/tasks");
+  if (!res.ok) throw new Error("Failed to fetch data");
+  return json(await res.json());
+};
+
 export default function Index() {
-  return <div>Remix Frontend - OK</div>;
+  const { data: tasks } = useLoaderData<typeof loader>();
+  return (
+    <div>
+      Remix Frontend - OK
+      <h1>Tasks</h1>
+      <ul>
+        {tasks &&
+          tasks.map((task, id) => (
+            <li key={`${task.title}-${id}`}>{task.title}</li>
+          ))}
+      </ul>
+    </div>
+  );
 }
